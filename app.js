c73f11fa90dbbc1fb682538c3bf219a248617d5a
sigrundish <sigrun@dohop.com>
2018-02-27 12:41:00 +0000

    app.js fix

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const users = require('./routes/usersRoute');
+const books = require('./routes/booksRoute');
+const categories = require('./routes/categoriesRoute');
+
+const app = express();
+
+app.use(express.json());
+app.use('/users', users);
+app.use('/books', books);
+app.use('/categories', categories);
+
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  res.status(404).json({ error: 'Not found' });
+}
+
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
+
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid json' });
+  }
+
+  return res.status(500).json({ error: 'Internal server error' });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const {
+  PORT: port = 3000,
+  HOST: host = '127.0.0.1',
+} = process.env;
+
+app.listen(port, () => {
+  console.info(`Server running at http://${host}:${port}/`);
+});
